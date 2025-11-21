9abc56f3d4a05fbfe7be1c7089c81830a4324a2f
maxdcs <maks.drzy@gmail.com>
2025-11-21 18:34:34 +0100

    userDesk and userPermission endpoint

--- src/routes/userDeskRoutes.ts
+++ src/routes/userDeskRoutes.ts
@@ -3,7 +3,20 @@ import { getAllUserDesks } from "../controllers/userDeskController"
 
 const router = Router()
 
+import {
+  getUserDeskById,
+  createUserDesk,
+  updateUserDesk,
+  deleteUserDesk,
+} from "../controllers/userDeskController"
+
 // GET /api/user-desks
 router.get("/", getAllUserDesks)
+
+// Single resource
+router.get("/:id", getUserDeskById)
+router.post("/", createUserDesk)
+router.put("/:id", updateUserDesk)
+router.delete("/:id", deleteUserDesk)
 
 export default router
